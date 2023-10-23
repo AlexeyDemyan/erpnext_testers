474f9dc0fe4f5b41402c991254554cc0d7499d62
Alexey Demyanov <deal110988@him-mail.ch>
2023-10-23 16:33:50 +0200

    need to re-arrange the order of items in output

--- src/shared/helpers/common/generate-csv-file.ts
+++ src/shared/helpers/common/generate-csv-file.ts
@@ -1,26 +1,47 @@
-import { dataForFixedHeaders } from "../index.js";
+import { dataForFixedHeaders, stockEntryDictionary } from "../index.js";
 
 export function generateOutputCSVFileFromArray(
   columnsToTake: string[],
   itemLines: string[][],
   columnNumbers: number[]
 ): string {
-
-  let totalOutput = '';
+  let firstLine: string[] = [];
+  let totalOutput: string[] = [];
+  let headersToInsert: string[] = [];
 
   for (let column in columnsToTake) {
     if (Object.keys(dataForFixedHeaders).includes(columnsToTake[column])) {
-      totalOutput+= `"${columnsToTake[column]}",`;
-      console.log(`key of ${columnsToTake[column]} is ${dataForFixedHeaders[columnsToTake[column]]}`)
+      firstLine.push(columnsToTake[column]);
+      headersToInsert.push(dataForFixedHeaders[columnsToTake[column]]);
+    } else if (
+      Object.values(stockEntryDictionary).includes(columnsToTake[column])
+    ) {
+      for (let entry of Object.entries(stockEntryDictionary)) {
+        if (entry[1] === columnsToTake[column]) {
+          firstLine.push(entry[0]);
+        }
+      }
+    } else {
+      console.error(`column is missing in all constant dictionaries`);
     }
   }
 
-  console.log(`columnsToTake in generateOutputCSVFileFromArray`);
-  console.log(columnsToTake);
-  console.log(`first line of items in generateOutputCSVFileFromArray`);
-  console.log(itemLines[0])
-  console.log(`column numbers in generateOutputCSVFileFromArray`);
-  console.log(columnNumbers);
+  totalOutput.push(firstLine.map((entry) => `"${entry}"`).join(","));
 
-  return totalOutput;
+  let secondLine: string[] = [...headersToInsert];
+
+  columnNumbers.forEach((elt) => {
+    secondLine.push(itemLines[0][elt]);
+  })
+
+  totalOutput.push(secondLine.map((entry) => `"${entry}"`).join(","));
+
+  // console.log(`columnsToTake in generateOutputCSVFileFromArray`);
+  // console.log(columnsToTake);
+  // console.log(`first line of items in generateOutputCSVFileFromArray`);
+  // console.log(itemLines[0]);
+  // console.log(`column numbers in generateOutputCSVFileFromArray`);
+  // console.log(columnNumbers);
+
+  return totalOutput.map((line) => `${line}`).join("\n");
 }
